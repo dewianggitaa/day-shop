8858a8392fa6423acf4c788fd44fe62a3b41ffc6
@danggita000 <danggita000@gmail.com>
2024-09-08 21:50:26 +0700

    add simple and basic layout in homepage

--- src/Product.js
+++ src/Product.js
@@ -5,11 +5,14 @@ const Product = () => {
     return (
         <div>
             <div className="product">
-                <div className="title">Title</div>
-                <div className="price">$30</div>
-                <div className="rating">⭐⭐⭐</div>
-                <img src="https://images.unsplash.com/photo-1667411424594-403300e5cc35?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-                <button>SHOP</button>
+                <div className="desc">
+                    <p className="title">Title</p>
+                    <p className="info">Tas ini  menurpakan tas berbahan dasar kulit komodo yang dibuat</p>
+                    <p className="price">$30</p>
+                    <p className="rating">⭐⭐⭐</p>
+                </div>
+                <img className='image_product' src="https://images.unsplash.com/photo-1667411424594-403300e5cc35?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+                <button className='button_product'>SHOP</button>
             </div>
         </div>
     )
